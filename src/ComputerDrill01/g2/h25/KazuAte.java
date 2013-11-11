e16f3d46d2924f13882da6db8aec170c2b9f0afd
fujisawa <fujisawa@dhcp139.kanazawa-tc.ac.jp>
2013-11-11 20:47:31 +0900

    第２０週で使用するソースコード群

--- src/ComputerDrill01/g2/h25/KazuAte.java
+++ src/ComputerDrill01/g2/h25/KazuAte.java
@@ -2,23 +2,30 @@ package ComputerDrill01.g2.h25;
 
 public class KazuAte {
 	public static void main(String[] args) {
-		if( args.length > 0 ){
+		if( args.length == 0 ){
+			System.out.println("引数に正解を予想した数を指定して下さい．");
+		}else {
 			int seikai = -100;
 			String hikisu = args[0];
 			int kaitou = Integer.parseInt(hikisu);
 			
 			int offset = seikai - kaitou;
 			
-			if(true /* TODO: 書き換え */ ){
-				System.out.println("残念！もっと大きい数です．");
-			}
-			if(true /* TODO: 書き換え*/ ){
-				System.out.println("残念！もっと小さい数です.");
-			}
-			if( true /* TODO: 書き換え */ ){
+			if( offset > 0 ){
+				if( offset < 10 ){
+					System.out.println("残念！正解はもうちょっと大きい数です．");
+				}else{
+					System.out.println("残念！正解はずっと大きい数です．");
+				}
+			}else if( offset < 0  ){
+				if( offset > -10 ){
+					System.out.println("残念！正解はもうちょっと小さい数です.");
+				}else{
+					System.out.println("残念！正解はずっと小さい数です.");
+				}
+			}else{
 				System.out.println("正解！答えは " + hikisu + " でした.");
 			}
 		}
 	}
-
 }
