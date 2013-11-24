c244678b6e1ebe84116a6b2542a5adc3dea6f0e7
Takeshi Fujisawa <takeshi.fujisawa@gmail.com>
2013-11-24 19:24:35 +0900

    数当てプログラムで複数の引数を受け付けるように改造

--- src/ComputerDrill01/g2/h25/KazuAte.java
+++ src/ComputerDrill01/g2/h25/KazuAte.java
@@ -2,23 +2,37 @@ package ComputerDrill01.g2.h25;
 
 public class KazuAte {
 	public static void main(String[] args) {
-		if( args.length > 0 ){
-			int seikai = -100;
-			String hikisu = args[0];
-			int kaitou = Integer.parseInt(hikisu);
-			
-			int offset = seikai - kaitou;
-			
-			if(true /* TODO: 書き換え */ ){
-				System.out.println("残念！もっと大きい数です．");
-			}
-			if(true /* TODO: 書き換え*/ ){
-				System.out.println("残念！もっと小さい数です.");
-			}
-			if( true /* TODO: 書き換え */ ){
-				System.out.println("正解！答えは " + hikisu + " でした.");
+		if (args.length == 0) {
+			System.out.println("異常終了：引数を指定して下さい。");
+		} else {
+			int seikai = 74;
+			int count = 1;
+			for (int i = 0; i < args.length; i++, count++) {
+				String hikisu = args[i];
+				int kaitou = Integer.parseInt(hikisu);
+				int offset = 0;
+				if (seikai > kaitou) {
+					System.out.print("残念！もっと大きい数で、");
+					offset = seikai - kaitou;
+				} else if (seikai < kaitou) {
+					System.out.print("残念！もっと小さい数で、");
+					offset = kaitou - seikai; // 今度は解答から正解を引く
+				}
+
+				if (seikai == kaitou) {
+					System.out.println("正解！答えは " + hikisu + " でした.");
+					System.out.println(count + "回目で正解しました！");
+					break;
+				} else {
+					if (offset <= 10) {
+						System.out.println("差分は１０以下です。");
+					} else if (offset > 10 && offset < 50) {
+						System.out.println("差分は１１以上、４９以下です。");
+					} else {
+						System.out.println("差分は５０以上です。");
+					}
+				}
 			}
 		}
 	}
-
 }
